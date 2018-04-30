8666806f296a1d282c4b79036b45565f2198d1f9
Maydev <alice.maydev@gmail.com>
2018-04-30 10:45:26 +0900

    Update scripts

--- scripts/download.py
+++ scripts/download.py
@@ -1,3 +1,9 @@
+"""
+script for download OTB-100
+
+aurthor: Bae Jiun, Maybe
+"""
+
 import argparse
 from pathlib import Path
 from urllib import request
@@ -10,21 +16,29 @@ from bs4 import BeautifulSoup as bs
 
 
 def process(des, prefix, target):
-    def download(url, to):
-        print ('Downloading {} ...'.format(url))
-        request.urlretrieve(url, to)
+    """process each dataset
 
-    def extract(target, to):
-        print ('Extracting {} ...'.format(target))
+    download, extract and remove
+    """
+    def download(url, obj):
+        print('Downloading {} ...'.format(url))
+        request.urlretrieve(url, obj)
+
+    def extract(target, obj):
+        print('Extracting {} ...'.format(target))
         with ZipFile(target) as z:
-            z.extractall(to)
+            z.extractall(obj)
 
     path = Path(des) / Path(target).name
-    
+
     download(prefix + target, str(path))
     extract(str(path), des)
     remove(str(path))
-    return True
+    return str(path), True
+
+
+def complete(obj, result):
+    print (obj, result)
 
 
 def main(des, core, prefix='http://cvlab.hanyang.ac.kr/tracker_benchmark/'):
@@ -36,14 +50,10 @@ def main(des, core, prefix='http://cvlab.hanyang.ac.kr/tracker_benchmark/'):
             for tag in table.find_all('a'):
                 yield tag.get('href')
 
-
-
-    pool = Pool()
-    pool.map(partial(process, des, prefix), targets(prefix+'datasets.html'))
+    pool = Pool(core)
+    pool.map_async(partial(process, des, prefix), targets(prefix+'datasets.html'), callback=complete)
     pool.close()
     pool.join()
-    # result = sum(map(process, targets(prefix+'datasets.html')))
-    # print ('Total {} done', result)
 
 if __name__ == '__main__':
     # argument parser
