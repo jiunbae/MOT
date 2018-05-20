6dc7eee9c29b591c0d8bbb8c7b33957882a41e22
MaybeS <maytryark@gmail.com>
2018-05-21 00:45:15 +0900

    Update scripts.resizer

--- scripts/resizer.py
+++ scripts/resizer.py
@@ -3,24 +3,32 @@ from glob import glob
 from os import path
 from pathlib import Path
 from configparser import ConfigParser
+import multiprocessing as mp
+from functools import partial
 
 from PIL import Image
 
 SIZE = (960, 540)
 
-def main(input_dir, output_dir):
-    def parse(directory):
+def task(output, directory):
+    def parse():
         config = ConfigParser()
         config.read(path.join(directory, 'seqinfo.ini'))
         return config.get('Sequence', 'imDir'), config.get('Sequence', 'imExt')
 
-    for directory in glob(path.join(input_dir, '*')):
-        if not path.isdir(directory): continue
-        imgs, ext = parse(directory)
-        for file in glob(path.join(input_dir, directory, imgs, '*' + ext)):
-            p = path.join(output_dir, directory, imgs, file)
-            Path(p).mkdir(parents=True, exist_ok=True)
-            Image.open(file).resize(SIZE, Image.ANTIALIAS).save(p)
+    if not path.isdir(directory): return
+    imgs, ext = parse()
+    for file in glob(path.join(directory, imgs, '*' + ext)):
+        p = path.join(output, path.basename(directory), imgs, path.basename(file))
+        Path(path.dirname(p)).mkdir(parents=True, exist_ok=True)
+        Image.open(file).resize(SIZE, Image.ANTIALIAS).save(p)
+
+def main(input_dir, output_dir):
+    pool = mp.Pool(mp.cpu_count())
+    r = pool.map_async(partial(task, output_dir), glob(path.join(input_dir, '*')))
+    pool.close()
+    pool.join()
+    r.get()
 
 if __name__ == '__main__':
     # argument parser
