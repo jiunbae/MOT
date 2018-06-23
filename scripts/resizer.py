efff9001578fcd7b987201e1785038813036cefb
Maydev <alice.maydev@gmail.com>
2018-06-23 17:56:43 +0900

    Update scripts.resizer fix gt

--- scripts/resizer.py
+++ scripts/resizer.py
@@ -1,31 +1,42 @@
 import argparse
-from glob import glob
-from os import path
-from pathlib import Path
-from configparser import ConfigParser
 import multiprocessing as mp
+from configparser import ConfigParser
+from glob import glob
+from pathlib import Path
+from os import path
 from functools import partial
 
+import numpy as np
+import pandas as pd
 from PIL import Image
 
-SIZE = (960, 540)
+def task(file, to, size):
+    Image.open(file)\
+         .resize(size, Image.ANTIALIAS)\
+         .save(path.join(to, path.basename(file)))
 
-def task(output, directory):
-    def parse():
-        config = ConfigParser()
-        config.read(path.join(directory, 'seqinfo.ini'))
-        return config.get('Sequence', 'imDir'), config.get('Sequence', 'imExt')
+def main(input_dir, output_dir, ratio):
+    config = ConfigParser()
+    config.read(path.join(input_dir, 'seqinfo.ini'))
 
-    if not path.isdir(directory): return
-    imgs, ext = parse()
-    for file in glob(path.join(directory, imgs, '*' + ext)):
-        p = path.join(output, path.basename(directory), imgs, path.basename(file))
-        Path(path.dirname(p)).mkdir(parents=True, exist_ok=True)
-        Image.open(file).resize(SIZE, Image.ANTIALIAS).save(p)
+    if not path.isdir(input_dir): return
 
-def main(input_dir, output_dir):
+    size = np.array([int(config.get('Sequence', 'imWidth')),
+                     int(config.get('Sequence', 'imHeight'))]) * ratio
+    imDir = config.get('Sequence', 'imDir')
+    imExt = config.get('Sequence', 'imExt')
+
+    Path(path.join(output_dir, imDir)).mkdir(parents=True, exist_ok=True)
+    Path(path.join(output_dir, 'gt')).mkdir(parents=True, exist_ok=True)
+
+    # resize gt
+    df = pd.read_csv(path.join(input_dir, 'gt', 'gt.txt'), header=None)
+    df.values[:, 2:6] *= ratio
+    df.to_csv(path.join(output_dir, 'gt', 'gt.txt'), header=None, index=None)
+
+    # resize image
     pool = mp.Pool(mp.cpu_count())
-    r = pool.map_async(partial(task, output_dir), glob(path.join(input_dir, '*')))
+    r = pool.map_async(partial(task, to=path.join(output_dir, imDir), size=size), glob(path.join(input_dir, imDir, '*'+imExt)))
     pool.close()
     pool.join()
     r.get()
@@ -35,6 +46,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="input dir name", type=str)
     parser.add_argument("output", help="output dir name", type=str)
+    parser.add_argument("--size", help="resize ratio", type=float, default=.5)
 
     args = parser.parse_args()
-    main(args.input, args.output)
+    main(args.input, args.output, args.size)
