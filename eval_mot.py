89309d71d33a87bb2d935dfa7ccdbdeb901b8a13
jiun.bae <jiun.bae@webtoonscorp.com>
2018-10-02 11:01:22 +0900

    Update datasets path

--- eval_mot.py
+++ eval_mot.py
@@ -119,17 +119,7 @@ def main(data_root='/data/MOT16/train', det_root=None,
 
 
 if __name__ == '__main__':
-    import fire
-    fire.Fire(main)
-
-    # seqs_str = '''MOT16-02
-    #             MOT16-05
-    #             MOT16-09
-    #             MOT16-11
-    #             MOT16-13'''
-    # seqs = [seq.strip() for seq in seqs_str.split()]
-    #
-    # main(data_root='/data/MOT16/train',
-    #      seqs=seqs,
-    #      exp_name='mot16_val',
-    #      show_image=True)
+    main(data_root = '../datasets/MOT16/train',
+         seqs=['MOT16-02'],
+         exp_name='mot16_val',
+         show_image=False)
