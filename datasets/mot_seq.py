74801236b857db5b3895cd5f020855267e9f4cb4
jiun.bae <jiun.bae@webtoonscorp.com>
2018-10-19 16:52:23 +0900

    Remove unused parameters

--- datasets/mot_seq.py
+++ datasets/mot_seq.py
@@ -21,7 +21,6 @@ labels={'ped', ...			% 1
 };
 """
 
-
 def read_mot_results(filename, is_gt=False):
     labels = {1, 7, -1}
     targets = dict()
@@ -34,10 +33,6 @@ def read_mot_results(filename, is_gt=False):
                 fid = int(linelist[0])
                 targets.setdefault(fid, list())
 
-                if is_gt and ('MOT16-' in filename or 'MOT17-' in filename):
-                    label = int(float(linelist[-2])) if len(linelist) > 7 else -1
-                    if label not in labels:
-                        continue
                 tlwh = tuple(map(float, linelist[2:7]))
                 target_id = int(linelist[1])
 
@@ -45,38 +40,26 @@ def read_mot_results(filename, is_gt=False):
 
     return targets
 
-
 class MOTSeq(data.Dataset):
-    def __init__(self, root, det_root, seq_name, min_height, min_det_score):
+    def __init__(self, root, seq_name, min_score):
         self.root = root
         self.seq_name = seq_name
-        self.min_height = min_height
-        self.min_det_score = min_det_score
+        self.min_score = min_score
 
         self.im_root = os.path.join(self.root, self.seq_name, 'img1')
         self.im_names = sorted([name for name in os.listdir(self.im_root) if os.path.splitext(name)[-1] == '.jpg'])
 
-        if det_root is None:
-            self.det_file = os.path.join(self.root, self.seq_name, 'det', 'det.txt')
-        else:
-            self.det_file = os.path.join(det_root, '{}.txt'.format(self.seq_name))
+        self.det_file = os.path.join(self.root, self.seq_name, 'det', 'det.txt')
         self.dets = read_mot_results(self.det_file, is_gt=False)
 
         self.gt_file = os.path.join(self.root, self.seq_name, 'gt', 'gt.txt')
-        if os.path.isfile(self.gt_file):
-            self.gts = read_mot_results(self.gt_file, is_gt=True)
-        else:
-            self.gts = None
+        self.gts = read_mot_results(self.gt_file, is_gt=True) if os.path.isfile(self.gt_file) else None
 
     def __len__(self):
         return len(self.im_names)
 
     def __getitem__(self, i):
-        im_name = os.path.join(self.im_root, self.im_names[i])
-        # im = cv2.imread(im_name)
-        im = imread(im_name)  # rgb
-        im = im[:, :, ::-1]  # bgr
-
+        img = imread(os.path.join(self.im_root, self.im_names[i]))[:, :, ::-1] # bgr
 
         frame = i + 1
         dets = self.dets.get(frame, [])
@@ -85,29 +68,22 @@ class MOTSeq(data.Dataset):
         tlwhs = dets[:, 0:4]
         scores = dets[:, 4]
 
-        keep = (tlwhs[:, 3] >= self.min_height) & (scores > self.min_det_score)
+        keep = (scores > self.min_score)
         tlwhs = tlwhs[keep]
         scores = scores[keep]
         track_ids = np.asarray(track_ids, dtype=np.int)[keep]
 
+        gt_tlwhs, gt_ids = None, None
         if self.gts is not None:
             gts = self.gts.get(frame, [])
             gt_tlwhs, gt_ids = zip(*self.gts[frame]) if len(gts) > 0 else (np.empty([0, 5]), np.empty([0, 1]))
             gt_tlwhs = np.asarray(gt_tlwhs)
-            gt_tlwhs = gt_tlwhs[:, 0:4]
-        else:
-            gt_tlwhs, gt_ids = None, None
+            gt_tlwhs = gt_tlwhs[:, 0:4]            
 
-        return im, tlwhs, scores, gt_tlwhs, gt_ids
-
+        return img, tlwhs, scores, gt_tlwhs, gt_ids
 
 def collate_fn(data):
     return data[0]
 
-
-def get_loader(root, det_root, name, min_height=0, min_det_score=-np.inf, num_workers=3):
-    dataset = MOTSeq(root, det_root, name, min_height, min_det_score)
-
-    data_loader = data.DataLoader(dataset, 1, False, num_workers=num_workers, collate_fn=collate_fn)
-
-    return data_loader
+def get_loader(root, name, min_score=-np.inf, num_workers=3):
+    return data.DataLoader(MOTSeq(root, name, min_score), 1, False, num_workers=num_workers, collate_fn=collate_fn)
--- eval_mot.py
+++ eval_mot.py
@@ -7,15 +7,15 @@ def eval_seq(dataloader):
     tracker = OnlineTracker()
 
     for fid, batch in enumerate(dataloader):
-        frame, det_tlwhs, det_scores, _, _ = batch
+        frame, det_tlwhs, det_scores, gt_tlwhs, gt_id = batch
 
         for target in tracker.update(frame, det_tlwhs, None):
             yield fid+1, target.track_id, target.tlwh
 
-def main(data_root='/data/MOT16/train', seqs, det_root=None):
+def main(data_root, seqs):
     for seq in seqs:
         with open(path.join(data_root, seq, 'result.csv', 'w')) as f:
-            for fid, tid, tlwh in eval_seq(get_loader(data_root, det_root, seq)):
+            for fid, tid, tlwh in eval_seq(get_loader(data_root, seq)):
                 if tid < 0: continue
                 x, y, w, h = tlwh
                 f.write(','.join(map(str, [fid, tid, *tlwh]))+',1,-1,-1,-1\n')
