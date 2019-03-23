64aafd188bf13e1388d0aabae1e5a7bf871d3eb1
bae jiun, maybe <alice.maydev@gmail.com>
2019-03-23 20:51:38 +0900

    Remove nms

--- models/psroi_pooling/build.py
+++ models/psroi_pooling/build.py
@@ -2,32 +2,15 @@ import os
 import torch
 from torch.utils.ffi import create_extension
 
-sources = []
-headers = []
-defines = []
-with_cuda = False
-
-if torch.cuda.is_available():
-    print('Including CUDA code.')
-    sources += ['src/psroi_pooling_cuda.c']
-    headers += ['src/psroi_pooling_cuda.h']
-    defines += [('WITH_CUDA', None)]
-    with_cuda = True
-
-this_file = os.path.dirname(os.path.realpath(__file__))
-print(this_file)
-extra_objects = ['src/cuda/psroi_pooling.cu.o']
-extra_objects = [os.path.join(this_file, fname) for fname in extra_objects]
-
-ffi = create_extension(
-    '_ext.psroi_pooling',
-    headers=headers,
-    sources=sources,
-    define_macros=defines,
-    relative_to=__file__,
-    with_cuda=with_cuda,
-    extra_objects=extra_objects
-)
-
 if __name__ == '__main__':
-    ffi.build()
+    create_extension(
+        '_ext.psroi_pooling',
+        headers=['src/psroi_pooling_cuda.h'],
+        sources=['src/psroi_pooling_cuda.c'],
+        define_macros=[('WITH_CUDA', None)],
+        relative_to=__file__,
+        with_cuda=True,
+        extra_objects=[os.path.join(os.path.dirname(os.path.realpath(__file__)), fname) for fname in [
+            'src/cuda/psroi_pooling.cu.o'
+        ]]
+    ).build()
---
+++ utils/nms.py
@@ -0,0 +1,54 @@
+import torch
+import numpy as np
+
+def nms(boxes, scores, thresh):
+    boxes = torch.from_numpy(boxes)
+    scores = torch.from_numpy(scores)
+
+    keep = scores.new(scores.size(0)).zero_().long()
+    if boxes.numel() == 0:
+        return keep
+    x1 = boxes[:, 0]
+    y1 = boxes[:, 1]
+    x2 = boxes[:, 2]
+    y2 = boxes[:, 3]
+    area = torch.mul(x2 - x1, y2 - y1)
+    v, idx = scores.sort(0)
+    xx1 = boxes.new()
+    yy1 = boxes.new()
+    xx2 = boxes.new()
+    yy2 = boxes.new()
+    w = boxes.new()
+    h = boxes.new()
+
+    count = 0
+    while idx.numel() > 0:
+        i = idx[-1]
+        keep[count] = i
+        count += 1
+        if idx.size(0) == 1:
+            break
+        idx = idx[:-1]
+        torch.index_select(x1, 0, idx, out=xx1)
+        torch.index_select(y1, 0, idx, out=yy1)
+        torch.index_select(x2, 0, idx, out=xx2)
+        torch.index_select(y2, 0, idx, out=yy2)
+
+        xx1 = torch.clamp(xx1, min=x1[i])
+        yy1 = torch.clamp(yy1, min=y1[i])
+        xx2 = torch.clamp(xx2, max=x2[i])
+        yy2 = torch.clamp(yy2, max=y2[i])
+        w.resize_as_(xx2)
+        h.resize_as_(yy2)
+        w = xx2 - xx1
+        h = yy2 - yy1
+
+        w = torch.clamp(w, min=0.0)
+        h = torch.clamp(h, min=0.0)
+        inter = w*h
+
+        rem_areas = torch.index_select(area, 0, idx)
+        union = (rem_areas - inter) + area[i]
+        IoU = inter/union
+        idx = idx[IoU.le(overlap)]
+    return keep
